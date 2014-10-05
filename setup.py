89024412a2ece5f8b770fdabf6aa98e005f45e57
Mike Kaplinskiy <mikekap@vineapp.com>
2014-10-05 14:16:27 -0400

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, Extension
-import shutil
-import sys
 
 module1 = Extension('unicodedata2',
                     sources = ['./unicodedata2/unicodedata.c'],
                     include_dirs = ['./unicodedata2'],
-                    extra_compile_args=['-flto'],
-                    extra_link_args=['-flto'],
 )
-					
-setup (name = 'unicodedata2',
+
+setup (name = "unicodedata2",
        version = "7.0.0",
-       description = "Unicodedata, but updated to the current unicode version.",
+       description = "Unicodedata backport for python 2 updated to the latest unicode version.",
        ext_modules = [module1],
        author="Mike Kaplinskiy",
        author_email="mike.kaplinskiy@gmail.com",
        download_url="http://github.com/mikekap/unicodedata2",
-       license="BSD License",
+       license="Apache License 2.0",
        platforms=['any'],
        url="http://github.com/mikekap/unicodedata2",
 )
