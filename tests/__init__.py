ec347077baefcd7a2aaa0f0f0d0dfeeb7ffaa1c0
Mike Kaplinskiy <mkaplinskiy@twitter.com>
2015-09-15 01:03:07 -0400

    Attempt to fix windows CI.

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,8 +12,9 @@ try:
     UnicodeFunctionsTest.expectedchecksum = '97c2dafb4c8258847339c8def763c3618d242c87'
 except ImportError as e:
     print 'Could not import python tests'
-
-
+else:
+    if sys.version_info < (2, 7):
+        del UnicodeFunctionsTest.test_function_checksum  # The checksums are different on 2.6
 
 
 class TestUnidataVersion(TestCase):
