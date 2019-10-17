08a3a4857d54ab56f8cff6320fabd2f566d5e3db
Cosimo Lupo <clupo@google.com>
2019-10-17 12:53:07 +0100

    reformat setup.py with black

--- setup.py
+++ setup.py
@@ -2,25 +2,29 @@ import sys
 from setuptools import setup, Extension
 
 if sys.version_info < (3,):
-    type = 'py2'
+    type = "py2"
 else:
-    type = 'py3'
+    type = "py3"
 
-module1 = Extension('unicodedata2',
-                    sources = ['./unicodedata2/' + type + '/unicodedata.c',
-                               './unicodedata2/unicodectype.c'],
-                    include_dirs = ['./unicodedata2/' + type, './unicodedata2/'],
+module1 = Extension(
+    "unicodedata2",
+    sources=[
+        "./unicodedata2/" + type + "/unicodedata.c",
+        "./unicodedata2/unicodectype.c",
+    ],
+    include_dirs=["./unicodedata2/" + type, "./unicodedata2/"],
 )
 
-setup (name = "unicodedata2",
-       version = "12.1.0",
-       description = "Unicodedata backport for Python 2/3 updated to the latest Unicode version.",
-       ext_modules = [module1],
-       author="Mike Kaplinskiy",
-       author_email="mike.kaplinskiy@gmail.com",
-       download_url="http://github.com/mikekap/unicodedata2",
-       license="Apache License 2.0",
-       platforms=['any'],
-       url="http://github.com/mikekap/unicodedata2",
-       test_suite="tests",
+setup(
+    name="unicodedata2",
+    version="12.1.0",
+    description="Unicodedata backport for Python 2/3 updated to the latest Unicode version.",
+    ext_modules=[module1],
+    author="Mike Kaplinskiy",
+    author_email="mike.kaplinskiy@gmail.com",
+    download_url="http://github.com/mikekap/unicodedata2",
+    license="Apache License 2.0",
+    platforms=["any"],
+    url="http://github.com/mikekap/unicodedata2",
+    test_suite="tests",
 )
