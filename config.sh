60289950d1918a4487a3a6eab8ce4c2ebefd3745
Cosimo Lupo <clupo@google.com>
2020-03-20 10:52:53 +0000

    travis: run tests using tox
    since we use multibuild to build the wheels for Linux inside a different Docker image than the one used to run the tests, we want to run the tests against the pre-compiled wheel package, NOT re-build it from source from within the test environment; thus we use 'tox --installpkg' option here.

--- config.sh
+++ config.sh
@@ -8,9 +8,21 @@ function pre_build {
 }
 
 function run_tests {
+    # The function is called from an empty temporary directory.
+    cd ..
+
     # check we have the expected version and architecture for Python
     python -c "import sys; print(sys.version)"
     python -c "import struct; print(struct.calcsize('P') * 8)"
-    # run the test suite
-    python ../tests/test_unicodedata2.py -v
+
+    # Get absolute path to the pre-compiled wheel
+    wheelhouse=$(abspath wheelhouse)
+    wheel=$(ls ${wheelhouse}/unicodedata2*.whl | head -n 1)
+    if [ ! -e "${wheel}" ]; then
+        echo "error: can't find wheel in ${wheelhouse} folder" 1>&2
+        exit 1
+    fi
+
+    # Install pre-compiled wheel and run tests against it
+    tox --installpkg "${wheel}"
 }
