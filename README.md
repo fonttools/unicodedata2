d10ce5cc9bad750a6cd88ed33d81874b705a134c
Harmon <Harmon758@gmail.com>
2021-10-14 09:55:49 -0500

    Drop support for Python 2 (#48)

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 unicodedata2
 ============
 
-[unicodedata] backport/updates to Python 3 and Python 2.
+[unicodedata] backport/updates.
 
 The versions of this package match Unicode versions, so unicodedata2==13.0.0 is data from Unicode 13.0.0.
-Additionally this backports support for named aliases and named sequences to Python 2.
 
 Pre-compiled wheel packages are available on [PyPI] and can be installed via pip.
 
--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-import io
 import sys
 from setuptools import setup, Extension
 
-if sys.version_info < (3,):
-    type = "py2"
-else:
-    type = "py3"
-
 # concatenate README.md and CHANGELOG.md into long_description so they are
 # displayed on the unicodedata2 project page on PyPI
-with io.open("README.md", "r", encoding="utf-8") as readme:
+with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 long_description += "\nChangelog\n=========\n"
-with io.open("CHANGELOG.md", "r", encoding="utf-8") as changelog:
+with open("CHANGELOG.md", "r", encoding="utf-8") as changelog:
     long_description += changelog.read()
 
 module_sources = [
-    "./unicodedata2/" + type + "/unicodedata.c",
+    "./unicodedata2/unicodedata.c",
     "./unicodedata2/unicodectype.c",
 ]
 
@@ -27,13 +21,13 @@ if is_pypy:
 module1 = Extension(
     "unicodedata2",
     sources=module_sources,
-    include_dirs=["./unicodedata2/" + type, "./unicodedata2/"],
+    include_dirs=["./unicodedata2/"],
 )
 
 setup(
     name="unicodedata2",
     version="13.0.0-2",
-    description="Unicodedata backport for Python 2/3 updated to the latest Unicode version.",
+    description="Unicodedata backport updated to the latest Unicode version.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     ext_modules=[module1],
