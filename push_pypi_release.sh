f201ebeb8321f8911b0bf9a762cb1fb2a751a3ed
Khaled Hosny <khaled@aliftype.com>
2021-12-13 12:39:26 +0200

    Add GitHub Actions setup (#51)
    * Add GitHub Actions setup
    
    Adapted from https://github.com/fonttools/skia-pathops/blob/ec787d4ea4824c3b5628ca4064b069519d0b44e2/.github/workflows/ci.yml
    
    * Add testing extra
    
    * Better check for tag builds
    
    * Pre-releases are not applicable here
    
    * Remove old CI setup files
    
    * Update badges [ci skip]

--- push_pypi_release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -rf dist || true
-mkdir -p dist
-curl -s https://api.github.com/repos/fonttools/unicodedata2/releases/latest | jq -r '.assets[].browser_download_url' | xargs wget -P dist/ -nv
-twine upload dist/*
