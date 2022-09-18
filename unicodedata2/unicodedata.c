b9522a0799a3c9dacea79c1c7dcdf26c30535f6d
James Gerity <snoopjedi@gmail.com>
2022-09-18 01:02:35 -0400

    Add compatibility header

--- unicodedata2/unicodedata.c
+++ unicodedata2/unicodedata.c
@@ -27,15 +27,7 @@
 #include "structmember.h"
 #include "unicodectype.h"
 
-/* ----------------------------------------------------------------------- *
- * Update for Python 3.11 - Contributed by Victor Stinner in bpo-39573.
- * Compatibility macro for older Python versions.
- * ----------------------------------------------------------------------- */
-#if PY_VERSION_HEX < 0x030900A4 && !defined(Py_SET_TYPE)
-static inline void _Py_SET_TYPE(PyObject *ob, PyTypeObject *type)
-{ ob->ob_type = type; }
-#define Py_SET_TYPE(ob, type) _Py_SET_TYPE((PyObject*)(ob), type)
-#endif
+#include "_unicodedata2_compat.h"
 
 /*[clinic input]
 module unicodedata
@@ -119,10 +111,6 @@ new_previous_version(const char*name, const change_record* (*getrecord)(Py_UCS4)
         return (PyObject*)self;
 }
 
-
-#ifdef PYPY_VERSION
-#include "pypy_ctype.h"
-#endif
 
 /* --- Module API --------------------------------------------------------- */
 
