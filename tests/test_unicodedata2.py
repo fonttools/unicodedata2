f98d6e50b5a39ec3facf7a5e62a985d1bc48233f
James Gerity <snoopjedi@gmail.com>
2022-09-18 18:25:20 -0400

    Synchronize test suite with CPython v3.7.14
    Upstream changes from:
    https://github.com/python/cpython/commit/93cbca33f2ab8dd8ed0f8582d94ed1a65fd137a4#diff-c31ff7b8fca97de6b4fdaca3e14da27ab3cac411653e9c510a5378b189f909ea (see https://bugs.python.org/issue3811 )
    https://github.com/python/cpython/commit/1b08b30743ec823cc01a4efbe44b721986e5cb51#diff-c31ff7b8fca97de6b4fdaca3e14da27ab3cac411653e9c510a5378b189f909ea (see https://bugs.python.org/issue5828 )
    https://github.com/python/cpython/commit/71efeb7cbf9645815f6bc565dee1f0787359356a#diff-c31ff7b8fca97de6b4fdaca3e14da27ab3cac411653e9c510a5378b189f909ea (see https://bugs.python.org/issue4971 )
    https://github.com/python/cpython/commit/806d8cf0e8056726580e210e1dea960d6e77c910#diff-c31ff7b8fca97de6b4fdaca3e14da27ab3cac411653e9c510a5378b189f909ea (see https://bugs.python.org/issue7643 )

--- tests/test_unicodedata2.py
+++ tests/test_unicodedata2.py
@@ -6,11 +6,10 @@
 
 """
 
-from __future__ import unicode_literals
-
 import sys
 import unittest
 import hashlib
+from test.support import script_helper
 
 encoding = 'utf-8'
 errors = 'surrogatepass'
@@ -52,6 +51,8 @@ if sys.maxunicode < MAX_UNICODE_UCS4:
 
 ### Run tests
 
+# NOTE: UnicodeMethodsTest upstream tests methods on `str` objects, and
+# is excluded from the unicodedata2 suite
 class UnicodeDatabaseTest(unittest.TestCase):
 
     def setUp(self):
@@ -73,7 +74,7 @@ class UnicodeFunctionsTest(UnicodeDatabaseTest):
         data = []
         h = hashlib.sha1()
 
-        for i in range(MAX_UNICODE_UCS4 + 1):
+        for i in range(0x10000):
             char = chr(i)
             data = [
                 # Properties
@@ -179,7 +180,7 @@ class UnicodeFunctionsTest(UnicodeDatabaseTest):
         # which requires an external file.
 
     def test_pr29(self):
-        # https://www.unicode.org/review/pr-29.html
+        # http://www.unicode.org/review/pr-29.html
         # See issues #1054943 and #10254.
         composed = ("\u0b47\u0300\u0b3e", "\u1100\u0300\u1161",
                     'Li\u030dt-s\u1e73\u0301',
@@ -229,6 +230,23 @@ class UnicodeFunctionsTest(UnicodeDatabaseTest):
 
 class UnicodeMiscTest(UnicodeDatabaseTest):
 
+    # NOTE: this test is specific to CPython and is disabled in unicodedata2
+#     def test_failed_import_during_compiling(self):
+#         # Issue 4367
+#         # Decoding \N escapes requires the unicodedata module. If it can't be
+#         # imported, we shouldn't segfault.
+#
+#         # This program should raise a SyntaxError in the eval.
+#         code = "import sys;" \
+#             "sys.modules['unicodedata'] = None;" \
+#             """eval("'\\\\N{SOFT HYPHEN}'")"""
+#         # We use a separate process because the unicodedata module may already
+#         # have been loaded in this process.
+#         result = script_helper.assert_python_failure("-c", code)
+#         error = "SyntaxError: (unicode error) \\N escapes not supported " \
+#             "(can't load unicodedata module)"
+#         self.assertIn(error, result.err.decode("ascii"))
+
     def test_decimal_numeric_consistent(self):
         # Test that decimal and numeric are consistent,
         # i.e. if a character has a decimal value,
@@ -258,6 +276,44 @@ class UnicodeMiscTest(UnicodeDatabaseTest):
     def test_bug_1704793(self):
         self.assertEqual(self.db.lookup("GOTHIC LETTER FAIHU"), '\U00010346')
 
+    def test_ucd_510(self):
+        import unicodedata2
+        # In UCD 5.1.0, a mirrored property changed wrt. UCD 3.2.0
+        self.assertTrue(unicodedata2.mirrored("\u0f3a"))
+        self.assertTrue(not unicodedata2.ucd_3_2_0.mirrored("\u0f3a"))
+        # Also, we now have two ways of representing
+        # the upper-case mapping: as delta, or as absolute value
+        self.assertTrue("a".upper()=='A')
+        self.assertTrue("\u1d79".upper()=='\ua77d')
+        self.assertTrue(".".upper()=='.')
+
+    def test_bug_5828(self):
+        self.assertEqual("\u1d79".lower(), "\u1d79")
+        # Only U+0000 should have U+0000 as its upper/lower/titlecase variant
+        self.assertEqual(
+            [
+                c for c in range(sys.maxunicode+1)
+                if "\x00" in chr(c).lower()+chr(c).upper()+chr(c).title()
+            ],
+            [0]
+        )
+
+    def test_bug_4971(self):
+        # LETTER DZ WITH CARON: DZ, Dz, dz
+        self.assertEqual("\u01c4".title(), "\u01c5")
+        self.assertEqual("\u01c5".title(), "\u01c5")
+        self.assertEqual("\u01c6".title(), "\u01c5")
+
+    def test_linebreak_7643(self):
+        for i in range(0x10000):
+            lines = (chr(i) + 'A').splitlines()
+            if i in (0x0a, 0x0b, 0x0c, 0x0d, 0x85,
+                     0x1c, 0x1d, 0x1e, 0x2028, 0x2029):
+                self.assertEqual(len(lines), 2,
+                                 r"\u%.4x should be a linebreak" % i)
+            else:
+                self.assertEqual(len(lines), 1,
+                                 r"\u%.4x should not be a linebreak" % i)
+
 if __name__ == "__main__":
     unittest.main()
-
